d8a1eea5df1c8bceb771d0c66bd4739246526fb3
unknown <rmtestard@gmail.com>
2019-09-22 10:18:13 +0200

    I have created a form in the template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { TabsComponent } from './components/tabs/tabs.component';
 import { ListComponent } from './components/list/list.component';
 import { ItemComponent } from './components/item/item.component';
+import { CreateCharacterComponent } from './components/create-character/create-character.component';
 
 @NgModule({
-  declarations: [AppComponent, TabsComponent, ListComponent, ItemComponent],
+  declarations: [AppComponent, TabsComponent, ListComponent, ItemComponent, CreateCharacterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [StarWarsService, LogService],
   bootstrap: [AppComponent]
